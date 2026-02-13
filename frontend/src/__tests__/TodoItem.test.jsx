5b7b35368801ad64934939b4f9ca96a931dc7aea
KhajonvitP <coding015ichigo@gmail.com>
2026-02-13 14:08:49 +0700

    test add comment

--- frontend/src/__tests__/TodoItem.test.jsx
+++ frontend/src/__tests__/TodoItem.test.jsx
@@ -2,19 +2,28 @@ import { render, screen } from '@testing-library/react'
 import { expect } from 'vitest'
 import App from '../App.jsx'
 
-const baseTodo = [{             
+const baseTodo = {             
   id: 1,
   title: 'Sample Todo',
   done: false,
   comments: [],
-}];
+};
 
 
 describe('App', () => {
   it('renders with no comments correctly', () => {
+    const todoWithComment = [{
+      ...baseTodo,
+      comments: [
+        {id: 1, message: 'First comment'},
+        {id: 2, message: 'Another comment'},
+      ]
+    }];
     render(
-        <App init_todo={baseTodo}/>
+        <App init_todo={todoWithComment}/>
     );
     expect(screen.getByText('Sample Todo')).toBeInTheDocument();
+    expect(screen.getByText('First comment')).toBeInTheDocument();
+    expect(screen.getByText('Another comment')).toBeInTheDocument();
   });
 });
